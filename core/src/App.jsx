18982d752746e894d894cbf523da26a1764ff8e3
hamid kamyab <hamidkamyab1377@gmail.com>
2022-10-22 14:25:24 +0330

    Add the Routing system to Project

--- core/src/App.jsx
+++ core/src/App.jsx
@@ -1,43 +1,19 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
-import {useDispatch} from "react-redux"
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./index.css"
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
-import { usePersonStore, PersonStoreProvider } from "personStore/personStore";
+import { PersonStoreProvider } from "personStore/personStore";
+import { BrowserRouter } from "react-router-dom";
 
-const App = () => {
-  const {name, age, changeName, changeAge} = usePersonStore();
-
-  const nameChangeHandler = e => {
-    changeName(`${e.target.value}`)
-  }
-
-  const ageChangeHandler = e => {
-    changeAge(e.target.value)
-  }
-
-  return (
-    <>
-      <Header />
-      <Sidebar />
-
-      <div className="content">
-        <span className="d-block">
-          {`Hello ${name}, You're ${age} years old, cool !`}
-        </span>
-        <input type="text" placeholder="name" onChange={nameChangeHandler}/>
-        <input type="text" placeholder="age" onChange={ageChangeHandler}/>
-      </div>
-    </>
-  )
-}
+import CoreContent from "./CoreContent";
 
 ReactDOM.render(
-  <PersonStoreProvider>
-    <App />
-  </PersonStoreProvider>,
-document.getElementById("app"));
+  <BrowserRouter>
+    <PersonStoreProvider>
+      <CoreContent />
+    </PersonStoreProvider>
+  </BrowserRouter>,
+  document.getElementById("app")
+);
---
+++ core/src/CoreContent.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { Route, Routes } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
+const CoreContent = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Sidebar />
+
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<div>Hello Home</div>} />
+        </Routes>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default CoreContent;
